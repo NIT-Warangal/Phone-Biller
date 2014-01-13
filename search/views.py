8ad87b86a5c727eb1822453e84371ed5462d1441
Rashid Khan <rashood.khan@gmail.com>
2014-01-13 15:34:55 +0530

    Updated the View with the search function

--- search/views.py
+++ search/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect,render_to_response
+from django.template import Context,loader
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from django import forms
+from search.models import EmployeeDetails
+
+def search(request):
+	employees = EmployeeDetails.objects.all()
+
+	if request.method == 'POST':
+		key_id = request.POST['id']
+
+		if key_id != '':
+			employees = employees.filter(emp_id__contains=key_id)
+
+	employees = employees.order_by('emp_id')[:100]
+
+	context = {'employees' : employees}
+	return render(request,'search/index.html', context)
