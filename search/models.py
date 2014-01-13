b8f020279017abde88ede6e002ee5367a7422988
Rashid Khan <rashood.khan@gmail.com>
2014-01-13 15:25:47 +0530

    Updated the model

--- search/models.py
+++ search/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeDetails(models.Model):
+	emp_id = models.CharField(max_length=10)
+	emp_name = models.CharField(max_length=50)
+	emp_phone = models.CharField(max_length=15)
+	emp_bill = models.CharField(max_length=10)
+	emp_email = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.emp_name
