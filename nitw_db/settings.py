6d452c0afeb6afcfd204e1191bc636212f8cc24b
Rashid Khan <rashood.khan@gmail.com>
2014-01-13 15:49:03 +0530

    Updated the settings to host MySQL database

--- nitw_db/settings.py
+++ nitw_db/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,11 @@ WSGI_APPLICATION = 'nitw_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'nitw_search_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
