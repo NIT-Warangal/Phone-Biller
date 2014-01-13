6139e2d3daacf37a220543444e192a8b271a07e6
Rashid Khan <rashood.khan@gmail.com>
2014-01-13 16:32:25 +0530

    Added Static file support

--- nitw_db/urls.py
+++ nitw_db/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from search import views
@@ -11,4 +12,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include('search.urls', namespace='search'))
-)
+) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
